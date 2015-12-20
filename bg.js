4d671576e3b368a8cd50890373338b46e9f5853f
thewisenerd <thewisenerd@protonmail.com>
2015-12-20 23:53:15 +0530

    bg: refactor code
    add jsonp proxy, use regex, and use different tab hooks.
    
    Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

--- bg.js
+++ bg.js
@@ -3,7 +3,70 @@
  */
 var yifyChecker = {
 
-    apiURL : "https://yts.ag/api/v2/list_movies.json?query_term=",
+    apiURL    : "https://yts.ag/api/v2/list_movies.json?query_term=",
+
+    imdbRegex : /http:\/\/[\w]+\.imdb\.com\/title\/(tt[0-9]+)[^$]+$/,
+
+    /**
+     * https://github.com/afeld/jsonp
+     */
+    jsonproxy : "http://jsonp.thewisenerd.me/?url=",
+
+    /**
+     * Badge
+     */
+    Badge : {
+
+        // clear badge
+        clear : function() {
+            chrome.browserAction.setBadgeText({text:""});
+        },
+
+        // set badge
+        setBadge : function(str) {
+            chrome.browserAction.setBadgeText({text:str+""});
+            chrome.browserAction.setBadgeBackgroundColor({color:[204, 0, 0, 230]});
+        },
+
+    },
+
+    /**
+     * Storage
+     */
+    Storage : {
+
+        clear : function() {
+            chrome.storage.local.set({'html' : ''});
+        },
+
+        setText : function(str) {
+            chrome.storage.local.set({'html' : str});
+        },
+
+    },
+
+    /**
+     * Popup
+     */
+    Popup : {
+
+        // clear popup
+        clear : function() {
+            var views = chrome.extension.getViews({type: "popup"});
+            for (var i = 0; i < views.length; i++) {
+                views[i].document.innerHTML = '';
+            }
+        },
+
+        // set text
+        setText : function(str) {
+            var views = chrome.extension.getViews({type: "popup"});
+            for (var i = 0; i < views.length; i++) {
+                views[i].document.innerHTML = bodyText;
+            }
+        },
+
+    },
 
     run : function(imdb_id) {
         this.requestJSON(imdb_id);
@@ -14,7 +77,11 @@ var yifyChecker = {
      */
     requestJSON : function(imdb_id) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", this.apiURL + imdb_id, true);
+        var url = this.apiURL + imdb_id;
+        if (typeof(this.jsonproxy) !== 'undefined') {
+            url = this.jsonproxy + url;
+        }
+        xhr.open("GET", url, true);
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4) {
                 var resp = JSON.parse(xhr.responseText);
@@ -29,10 +96,11 @@ var yifyChecker = {
      */
     showTorrents : function(data) {
         var bodyText = '';
-        if(data.movie_count === 0){
+        if(data.movie_count === 0) {
             bodyText += "No torrent found :(";
-            // remove badge
-            chrome.browserAction.setBadgeText({text:""});
+
+            // say zero
+            this.Badge.setBadge("0");
         } else {
             var count = data.movies[0].torrents.length;
             // build link
@@ -40,31 +108,44 @@ var yifyChecker = {
                 var title = data.movies[0].title + ' - ' + data.movies[0].torrents[i].quality + ' (' + data.movies[0].torrents[i].size + ')';
                 bodyText += '<a href="'+data.movies[0].torrents[i].url+'" target="_blank">'+title+'</a>';
             }
-            // set badge count
-            chrome.browserAction.setBadgeBackgroundColor({color:[204, 0, 0, 230]});
-            chrome.browserAction.setBadgeText({text:count+""});
+            // say n
+            this.Badge.setBadge(count.toString());
         }
-        // save the value into local storage to user in popup
-        chrome.storage.local.set({'html' : bodyText});
-        var views = chrome.extension.getViews({type: "popup"});
-        for (var i = 0; i < views.length; i++) {
-            views[i].document.innerHTML = '';
-            // write the result to popup just in case
-            views[i].document.innerHTML = bodyText;
-        }
-    }
+        this.Storage.setText(bodyText);
+
+        // just in case changes don't propagate
+        this.Popup.setText(bodyText);
+    },
 
 };
 
 /**
  * Listen page load event and run checker
  */
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    chrome.tabs.query({active:true}, function(selectedTab){
-        var url = selectedTab[0].url.split('/');
-        // run only for imdb movie pages
-        if((url[2] === 'imdb.com' || url[2] === 'www.imdb.com') && url[3] === 'title' && details.url === selectedTab[0].url){
-            yifyChecker.run(url[4]);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
+    chrome.tabs.query({'active': true}, function (activeTabs) {
+        var url = activeTabs[0].url;
+        var m;
+        if ((m = yifyChecker.imdbRegex.exec(url)) !== null) {
+            yifyChecker.run(m[1]);
+        } else {
+            yifyChecker.Badge.clear();
+            yifyChecker.Storage.clear();
+            yifyChecker.Popup.clear();
+        }
+    });
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function(tab){
+        var url = tab.url;
+        var m;
+        if ((m = yifyChecker.imdbRegex.exec(url)) !== null) {
+            yifyChecker.run(m[1]);
+        } else {
+            yifyChecker.Badge.clear();
+            yifyChecker.Storage.clear();
+            yifyChecker.Popup.clear();
         }
     });
 });
